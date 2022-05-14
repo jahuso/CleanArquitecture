03935f91cc49a6ac9026e5ab554dc89e6a554381
Jose Ahumada <jahumada@prodriver.com>
2022-05-14 16:32:53 -0500

    Dependency Injection Added

--- CleanArquitecture.Application/Behaviors/ValidationBehaviour.cs
+++ CleanArquitecture.Application/Behaviors/ValidationBehaviour.cs
@@ -1,14 +1,10 @@
 ﻿using FluentValidation;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ValidationException = CleanArquitecture.Application.Exceptions.ValidationException;
 
 namespace CleanArquitecture.Application.Behaviors
 {
-    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
 
         private readonly IEnumerable<IValidator<TRequest>> _validators;
@@ -18,14 +14,24 @@ namespace CleanArquitecture.Application.Behaviors
             _validators = validators;
         }
 
-        //public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-        //{
-        //    if (_validators.Any())
-        //    {
-        //        var context = new ValidationContext<TRequest>(request);
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
 
-        //        var validationResults = Task.WhenAll(_validators.Select(v => v.ValidateAsync(context,cancellationToken);
-        //    }
-        //}
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context,cancellationToken)));
+
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count > 0)
+                {
+                    throw new ValidationException(failures);
+                }
+
+            }
+
+            return await next();
+        }
     }
 }
